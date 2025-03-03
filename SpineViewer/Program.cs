eafc470fbe3a89ce7a5b6e071b332cd247d390ef
ww-rm <ww-rm@qq.com>
2025-03-03 23:24:18 +0800

    增加图标

--- SpineViewer/Program.cs
+++ SpineViewer/Program.cs
@@ -15,7 +15,7 @@ namespace SpineViewer
         static void Main()
         {
             InitializeLogConfiguration();
-            Logger.Info("Program Started.");
+            Logger.Info("Program Started");
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
