d4ac38c7ffa6405391b454807147eeb45049638f
ww-rm <ww-rm@qq.com>
2025-03-04 19:36:05 +0800

    增加spine4.0

--- SpineViewer/Spine/Version.cs
+++ SpineViewer/Spine/Version.cs
@@ -43,7 +43,7 @@ namespace SpineViewer.Spine
         [Description("v3.6.53")] V36 = 0x0306,
         [Description("v3.7.94")] V37 = 0x0307,
         [Description("v3.8.99")] V38 = 0x0308,
-        [Description("v4.0.x")] V40 = 0x0400,
+        [Description("v4.0.64")] V40 = 0x0400,
         [Description("v4.1.x")] V41 = 0x0401,
         [Description("v4.2.x")] V42 = 0x0402,
         [Description("v4.3.x")] V43 = 0x0403,
